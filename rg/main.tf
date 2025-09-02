7bcf41ccd215657f122c5b4ae9d14a0a85b4c584
inishisingh <inishi.singh1498@gmail.com>
2025-09-02 18:20:07 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "this" {
-  name     = "${var.location}-${var.environment}-${var.workload}-${var.department}-rg-am"
-  location = var.location
+  name     = "${var.location_slug}-${var.environment}-${var.workload}-${var.department}-rg-am"
+  location = var.location_slug
 }
