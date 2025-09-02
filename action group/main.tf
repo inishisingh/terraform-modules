bdc44dab8d9c29a29c91137d44e1a843ea5b14d5
inishisingh <inishi.singh1498@gmail.com>
2025-09-03 00:25:13 +0530

    Update main.tf

--- action group/main.tf
+++ action group/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_action_group" "this" {
   name     = "${var.location_cli}-${var.environment}-${var.workload}-${var.department}-ag-am"
   resource_group_name = var.resource_group_name
   short_name          = "${var.location_slug}${var.environment}agam"
-  location            = var.location
+  location            = var.location_cli
 
   dynamic "email_receiver" {
     for_each = var.email_receivers
