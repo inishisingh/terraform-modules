a5301babb126f122eea8352501132284f97bed5e
inishisingh <inishi.singh1498@gmail.com>
2025-09-03 00:22:02 +0530

    Update variables.tf

--- action group/variables.tf
+++ action group/variables.tf
@@ -1,19 +1,19 @@
-variable "name" {
-  description = "Name of the Action Group"
-  type        = string
+variable "environment" {
+  type = string
 }
 
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
+variable "workload" {
+  type = string
 }
 
-variable "short_name" {
-  description = "Short name for the Action Group"
-  type        = string
+variable "department" {
+  type = string
 }
 
-variable "location" {
-  description = "Azure region"
-  type        = string
+variable "location_cli" {
+  type = string
+}
+
+variable "location_slug" {
+  type = string
 }
