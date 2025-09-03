4a3d7a803a6a36175da80629f4d3033024b7fc1c
inishisingh <inishi.singh1498@gmail.com>
2025-09-03 11:56:33 +0530

    Update output.tf

--- action group/output.tf
+++ action group/output.tf
@@ -1,4 +1,4 @@
 output "action_group_id" {
   description = "The ID of the Action Group"
-  value       = azurerm_monitor_action_group.this.id
+  value       = azurerm_monitor_action_group.azurerm_ag.id
 }
