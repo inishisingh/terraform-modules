759f25fbd966e81076395370bd24a48ee5259edf
inishisingh <inishi.singh1498@gmail.com>
2025-09-03 11:56:18 +0530

    Update main.tf

--- action group/main.tf
+++ action group/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_monitor_action_group" "azurerm" {
+resource "azurerm_monitor_action_group" "azurerm_ag" {
   name     = "${var.location_cli}-${var.environment}-${var.workload}-${var.department}-ag-am"
   resource_group_name = var.resource_group_name
   short_name          = "${var.location_slug}${var.environment}agam"
