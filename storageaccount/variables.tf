3c84352992911bb0f122b07333a67323e6df0a5e
inishisingh <inishi.singh1498@gmail.com>
2025-09-04 14:41:38 +0530

    Update variables.tf

--- storageaccount/variables.tf
+++ storageaccount/variables.tf
@@ -18,25 +18,17 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
 variable "account_tier" {
   description = "Defines the Tier to use for this storage account (Standard or Premium)"
   type        = string
-  default     = "Standard"
 }
 
 variable "account_replication_type" {
   description = "Defines the type of replication to use for this storage account"
   type        = string
-  default     = "LRS"
 }
 
 variable "access_tier" {
   description = "Defines the access tier for BlobStorage and General Purpose v2 accounts (Hot or Cool)"
   type        = string
-  default     = "Hot"
 }
