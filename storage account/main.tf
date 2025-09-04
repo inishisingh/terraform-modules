44e3ec816f8ef88d9a3dacdd25ce76614bb7215f
inishisingh <inishi.singh1498@gmail.com>
2025-09-04 14:25:49 +0530

    Update main.tf

--- storage account/main.tf
+++ storage account/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "this" {
   name                     = "${var.location_cli}${var.environment}${var.workload}${var.department}saam"
   resource_group_name      = var.resource_group_name
-  location                 = var.location
+  location                 = var.location_cli
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   access_tier              = var.access_tier
