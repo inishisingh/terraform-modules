a26aa4c0238a219f3ec7e24c2cc889b9766442c5
inishisingh <inishi.singh1498@gmail.com>
2025-09-04 15:16:14 +0530

    Update output.tf

--- storageaccount/output.tf
+++ storageaccount/output.tf
@@ -8,7 +8,7 @@ output "storage_account_name" {
   value       = azurerm_storage_account.azurerm_sa.name
 }
 
-output "storage_account__conatiner_name" {
+output "storage_account_conatiner_name" {
   description = "The name of the storage account conatiner"
   value       = azurerm_storage_container.azure_rm_conatiner.name
 }
