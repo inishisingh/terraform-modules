f9b8482a31ae9ebc992e976c4ed0fe96dfa4c767
inishisingh <inishi.singh1498@gmail.com>
2026-02-17 16:10:35 +0530

    Update output.tf

--- law/output.tf
+++ law/output.tf
@@ -1,3 +1,9 @@
 output "law_id" {
   description = "The ID of the Law (log Analytics Workspcace)"
   value       = azurerm_log_analytics_workspace.azurerm_law.id
+}
+
+output "law_name" {
+  description = "The name of the Law (log Analytics Workspcace)"
+  value       = azurerm_log_analytics_workspace.azurerm_law.name
+}
