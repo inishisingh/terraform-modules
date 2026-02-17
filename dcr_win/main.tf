3cc85f3c5fb1d56a5c12c0f9a3a55da79b2ba986
inishisingh <inishi.singh1498@gmail.com>
2026-02-17 12:47:15 +0530

    Update main.tf

--- dcr_win/main.tf
+++ dcr_win/main.tf
@@ -1,32 +1,26 @@
-resource "azurerm_policy_definition" "ama_linux_by_tag" {
-  name         = "Deploy-AMA-and-DCRA-Linux-By-Tag"
-  display_name = "Deploy AMA + DCR Association for Linux VMs with tag monitoring"
-  description  = "Installs Azure Monitor Agent on Linux VMs and associates them to a DCR when tag 'monitoring' is present with allowed value"
+resource "azurerm_policy_definition" "ama_windows_by_tag" {
+  name         = "Deploy-AMA-and-DCRA-Windows-By-Tag"
+  display_name = "Deploy AMA + DCR Association for Windows VMs with tag monitoring"
+  description  = "Installs Azure Monitor Agent on Windows VMs and associates them to a DCR when tag 'monitoring' matches allowed values"
   policy_type  = "Custom"
   mode         = "Indexed"
 
-  metadata = jsonencode({
-    category = "Monitoring"
-  })
+  metadata = jsonencode({ category = "Monitoring" })
 
   parameters = jsonencode({
-    dcrResourceId = {
-      type        = "String"
-      metadata    = { displayName = "DCR Resource ID" }
-    }
+    dcrResourceId = { type = "String", metadata = { displayName = "DCR Resource ID" } }
     monitoringTagValues = {
-      type        = "Array"
-      metadata    = { displayName = "Allowed tag values for 'monitoring'" }
+      type         = "Array"
+      metadata     = { displayName = "Allowed tag values for 'monitoring'" }
       defaultValue = ["on","true","yes"]
     }
   })
 
-  # Policy rule: If Linux VM has tag monitoring in allowed values and doesn't have DCRA to the given DCR, deploy AMA + DCRA
   policy_rule = jsonencode({
     if = {
       allOf = [
         { field = "type", equals = "Microsoft.Compute/virtualMachines" },
-        { field = "Microsoft.Compute/virtualMachines/storageProfile/osDisk/osType", equals = "Linux" },
+        { field = "Microsoft.Compute/virtualMachines/storageProfile/osDisk/osType", equals = "Windows" },
         { anyOf = [
             for v in ["on","true","yes"] : {
               field   = "tags['monitoring']"
@@ -41,8 +35,7 @@ resource "azurerm_policy_definition" "ama_linux_by_tag" {
       details = {
         type              = "Microsoft.Insights/dataCollectionRuleAssociations"
         roleDefinitionIds = [
-          # VM extension + association needs these:
-          "/providers/Microsoft.Authorization/roleDefinitions/9980e02c-c2be-4d73-94e8-173b1dc7cf3c", # Virtual Machine Contributor
+          "/providers/Microsoft.Authorization/roleDefinitions/9980e02c-c2be-4d73-94e8-173b1dc7cf3c", # VM Contributor
           "/providers/Microsoft.Authorization/roleDefinitions/749f88d5-cbae-40b8-bcfc-e573ddc772fa", # Monitoring Contributor
           "/providers/Microsoft.Authorization/roleDefinitions/92aaf0da-9dab-42b6-94a3-d43ce8d16293"  # Log Analytics Contributor
         ]
@@ -56,18 +49,17 @@ resource "azurerm_policy_definition" "ama_linux_by_tag" {
           properties = {
             mode = "Incremental"
             template = {
-              "$schema"      = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
-              "contentVersion" = "1.0.0.0"
-              "parameters" = {
-                "vmName"       = { "type" = "string" }
-                "location"     = { "type" = "string" }
-                "dcrId"        = { "type" = "string" }
-              }
-              "variables" = {
-                "amaExtName" = "AzureMonitorLinuxAgent"
-              }
-              "resources" = [
-                # 1) Ensure AMA extension exists
+              "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+              "contentVersion": "1.0.0.0",
+              "parameters": {
+                "vmName":   { "type": "string" },
+                "location": { "type": "string" },
+                "dcrId":    { "type": "string" }
+              },
+              "variables": {
+                "amaExtName": "AzureMonitorWindowsAgent"
+              },
+              "resources": [
                 {
                   "type": "Microsoft.Compute/virtualMachines/extensions",
                   "name": "[concat(parameters('vmName'), '/', variables('amaExtName'))]",
@@ -75,13 +67,12 @@ resource "azurerm_policy_definition" "ama_linux_by_tag" {
                   "location": "[parameters('location')]",
                   "properties": {
                     "publisher": "Microsoft.Azure.Monitor",
-                    "type": "AzureMonitorLinuxAgent",
+                    "type": "AzureMonitorWindowsAgent",
                     "typeHandlerVersion": "1.0",
                     "autoUpgradeMinorVersion": true,
                     "enableAutomaticUpgrade": true
                   }
                 },
-                # 2) Ensure DCR Association exists (name can be any stable string)
                 {
                   "type": "Microsoft.Compute/virtualMachines/providers/dataCollectionRuleAssociations",
                   "name": "[concat(parameters('vmName'), '/Microsoft.Insights/', 'dcr-association')]",
@@ -95,9 +86,9 @@ resource "azurerm_policy_definition" "ama_linux_by_tag" {
                     "[resourceId('Microsoft.Compute/virtualMachines/extensions', parameters('vmName'), variables('amaExtName'))]"
                   ]
                 }
-              ]
-              "outputs" = {}
-            }
+              ],
+              "outputs": {}
+            },
             parameters = {
               vmName   = { "value" = "[field('name')]" }
               location = { "value" = "[field('location')]" }
