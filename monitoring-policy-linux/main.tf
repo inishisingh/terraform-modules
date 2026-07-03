5db095c17f2018bbe9cc65633a10e05a944c3a9b
inishisingh <inishi.singh1498@gmail.com>
2026-07-03 13:24:51 +0530

    Update main.tf

--- monitoring-policy-linux/main.tf
+++ monitoring-policy-linux/main.tf
@@ -134,11 +134,11 @@ resource "azurerm_subscription_policy_assignment" "assign" {
       role_definition_id = local.role_vm_contributor
       principal_id       = each.value.identity[0].principal_id
 }
-    resource "azurerm_role_assignment" "pa_vm_contrib" {
-      for_each = azurerm_subscription_policy_assignment.assign
-      scope              = each.value.subscription_id
-      role_definition_id = local.role_vm_contributor
-      principal_id       = each.value.identity[0].principal_id
+    resource "azurerm_role_assignment" "pa_mon_contrib" {
+        for_each = azurerm_subscription_policy_assignment.assign
+        scope              = each.value.subscription_id
+        role_definition_id = local.role_monitoring_contributor
+        principal_id       = each.value.identity[0].principal_id
 }
     resource "azurerm_role_assignment" "pa_la_contrib" {
       for_each = azurerm_subscription_policy_assignment.assign
@@ -146,4 +146,3 @@ resource "azurerm_subscription_policy_assignment" "assign" {
       role_definition_id = local.role_vm_contributor
       principal_id       = each.value.identity[0].principal_id
 }
-    }
