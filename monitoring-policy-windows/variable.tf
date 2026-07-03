b2daa2ee37bc42a5fcb3cdd21d604c9c9a07ed77
inishisingh <inishi.singh1498@gmail.com>
2026-07-03 13:18:24 +0530

    Update variable.tf

--- monitoring-policy-windows/variable.tf
+++ monitoring-policy-windows/variable.tf
@@ -1,6 +1,7 @@
 variable "subscription_ids" {
   type = list(string)
 }
+variable "policy_assignment_scope_id" {}
 
 variable "dcr_resource_id" {
   description = "Resource ID of the Data Collection Rule to associate VMs with."
