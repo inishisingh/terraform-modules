1bfa14c52c6896823cf9b049a76f367a91673f7e
inishisingh <inishi.singh1498@gmail.com>
2026-07-03 12:18:35 +0530

    Update output.tf

--- monitoring-policy-linux/output.tf
+++ monitoring-policy-linux/output.tf
@@ -1,5 +1,5 @@
 
 output "policy_assignment_id" {
-  value       = azurerm_policy_assignment.assign.id
+  value       = azurerm_subscription_policy_assignment.assign.id
   description = "Resource ID of the policy assignment"
 }
